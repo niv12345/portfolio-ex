34159fd2f72b6f3c8e7fdddd6518ec9f3726f18f
Niv Kapade <niv.kapade02@gmail.com>
2025-02-08 17:51:06 +0530

    First comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'portfolio-ex';
+  nm = '';
+  em = '';
+  ph = '';
+  add = '';
+  submitted:boolean = false;
+  qualification = [{school:'',degree:'',year:''}];
+
+  addQuali(){
+    this.qualification.push({school:'',degree:'',year:''})
+  }
+  formSub(){
+    this.submitted = true;
+  }
+  foredit(){
+   this.submitted = false;
+  }
 }
